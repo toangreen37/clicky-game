3e7f11348f7b87441d5bcc89e5225852ff63b774
Turbo <toan.takahara@gmail.com>
2019-07-06 17:12:19 -0500

    Updates

---
+++ precache-manifest.945a4511ccfc1e52cc897de795e02bc1.js
@@ -0,0 +1,54 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6132dbb053fb984fae0d76524cfa6a79",
+    "url": "/clicky-game/index.html"
+  },
+  {
+    "revision": "1d17d6a7016de28989cf",
+    "url": "/clicky-game/static/css/main.8c30771c.chunk.css"
+  },
+  {
+    "revision": "f43c4bfee2815856d4c9",
+    "url": "/clicky-game/static/js/2.e9bf0032.chunk.js"
+  },
+  {
+    "revision": "1d17d6a7016de28989cf",
+    "url": "/clicky-game/static/js/main.9205b7a3.chunk.js"
+  },
+  {
+    "revision": "265538ab0e4606d350e6",
+    "url": "/clicky-game/static/js/runtime~main.27874bd3.js"
+  },
+  {
+    "revision": "38f2fc18108def3d38b526093def0c98",
+    "url": "/clicky-game/static/media/bg.38f2fc18.jpg"
+  },
+  {
+    "revision": "b25e1eca483fa5c7aab51545c772d39e",
+    "url": "/clicky-game/static/media/bulldog.b25e1eca.jpg"
+  },
+  {
+    "revision": "2ae7caa83471ad1ac8e162ef181893fa",
+    "url": "/clicky-game/static/media/goldenretriever.2ae7caa8.jpg"
+  },
+  {
+    "revision": "d7d82486bf84b3f0b69c6342785fe23c",
+    "url": "/clicky-game/static/media/husky.d7d82486.jpg"
+  },
+  {
+    "revision": "a62332dc0a433b36a164c7371dbcfd3d",
+    "url": "/clicky-game/static/media/lab.a62332dc.jpg"
+  },
+  {
+    "revision": "0ccd054c1cb97cde9023284112f0a60a",
+    "url": "/clicky-game/static/media/maltese.0ccd054c.jpg"
+  },
+  {
+    "revision": "96181448a23b6271ae78bc7d61f08657",
+    "url": "/clicky-game/static/media/samoyed.96181448.jpg"
+  },
+  {
+    "revision": "68996af8c34dcb14df3e2b1b193232b9",
+    "url": "/clicky-game/static/media/yorkie.68996af8.jpg"
+  }
+]);
